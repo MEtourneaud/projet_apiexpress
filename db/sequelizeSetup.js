912440e3d2ee6e299633fd57b34a38e52d806771
Mathieu Etourneaud <m.etourneaud@gmail.com>
2023-12-30 12:15:11 +0100

    Export sequelize in sequelizeSetup

--- db/sequelizeSetup.js
+++ db/sequelizeSetup.js
@@ -51,4 +51,4 @@ sequelize
   .then(() => console.log("La connexion à la base de données a bien été établie."))
   .catch((error) => console.error(`Impossible de se connecter à la base de données ${error}`))
 
-module.exports = { User, Role, Manga, Review, Status }
+module.exports = { User, Role, Manga, Review, Status, sequelize }
